6ab038dc06b5ce6365590470f12056cc21d595b5
Lucas de Paula <45231571+lucasdepaula19@users.noreply.github.com>
2019-11-21 00:30:19 -0300

    conf controllers

--- app/controllers/ExpenseController.js
+++ app/controllers/ExpenseController.js
@@ -5,35 +5,102 @@ const db = knex(knexConfigs.development)
 const TABLE_NAME = 'tb_expense'
 
 module.exports = {
-    get() {
-        return db(TABLE_NAME).select('*')
+
+    async getExpense() {
+        let objRetorno = {};
+        objRetorno.status = false;
+        objRetorno.code = 500;
+
+        await db.select().table(TABLE_NAME)
+            .then(result => {
+                objRetorno.code = 200;
+                objRetorno.status = true;
+                objRetorno.data = result;
+            })
+            .catch(err => {
+                objRetorno.data = err;
+            });
+
+        return objRetorno;
     },
 
-    getExpense(expense) {
-        let expenseId = expense.id;
-        return db(TABLE_NAME).where('id', expenseId).select('');
+    async getExpenseId(expenseId) {
+        let objRetorno = {};
+        objRetorno.status = false;
+        objRetorno.code = 500;
+
+        await db(TABLE_NAME)
+            .select('*')
+            .where('id', expenseId)
+            .then(result => {
+                objRetorno.code = 200;
+                objRetorno.status = true;
+                objRetorno.data = result;
+            })
+            .catch(err => {
+                objRetorno.data = err;
+            });
+
+        return objRetorno;
+    },
+
+    async getExpenseName(expenseName) {
+        let objRetorno = {};
+        console.log('expenseName: ' + JSON.stringify(expenseName));
+        await db(TABLE_NAME)
+            .select('id')
+            .where(expenseName)
+            .then(result => {
+                console.log('result getByName: ' + JSON.stringify(result));
+                objRetorno.data = result;
+            })
+            .catch(err => {
+                objRetorno.data = err;
+            });
+        console.log('objRetorno:' + JSON.stringify(objRetorno.data[0].id));
+        return objRetorno.data[0].id;
     },
 
     newExpense(expense) {
-        let obj = { name: expense.fname, due: expense.fdue, user: 'lucas.paula' }
-        return db.insert(obj).into(TABLE_NAME)
-        .then(function () {
-        });
+        let obj = { name: expense.fname, user: 'lucas.paula' }
+        //return db.insert(obj).into(TABLE_NAME)
+        return db(TABLE_NAME)
+            .insert(obj)
+            .then(function () {
+            });
     },
 
-    delExpense(expense) {
-        return db(TABLE_NAME)
-            .where('id', expense.id)
-            .del();
+    async delExpense(expense) {
+        let obj_name = { name: expense.fname };
+        let exit;
+        console.log('obj_name: ' + JSON.stringify(obj_name));
+
+        await this.getExpenseName(obj_name)
+            .then(result => {
+                console.log('result: ' + JSON.stringify(result));
+                let obj = { id: result };
+                console.log('obj: ' + JSON.stringify(obj));
+                exit = db(TABLE_NAME)
+                    .where(obj)
+                    .del()
+                    .then(function () {
+                    });
+            })
+            .catch(err => {
+                exit = err;
+            });
+        console.log('exit: ' + JSON.stringify(exit) );
+        return exit;
     },
 
     updateExpense(expense) {
         return db(TABLE_NAME)
             .where('id', expense.id)
             .update({
-                name: expense.nome,
-                description: expense.descricao,
-                price: expense.preco
+                name: expense.fname,
+                user: 'lucas.paula'
+            })
+            .then(function () {
             });
     }
 }
--- app/controllers/RevenueController.js
+++ app/controllers/RevenueController.js
@@ -5,35 +5,102 @@ const db = knex(knexConfigs.development)
 const TABLE_NAME = 'tb_revenue'
 
 module.exports = {
-    get() {
-        return db(TABLE_NAME).select('*')
+
+    async getRevenue() {
+        let objRetorno = {};
+        objRetorno.status = false;
+        objRetorno.code = 500;
+
+        await db.select().table(TABLE_NAME)
+            .then(result => {
+                objRetorno.code = 200;
+                objRetorno.status = true;
+                objRetorno.data = result;
+            })
+            .catch(err => {
+                objRetorno.data = err;
+            });
+
+        return objRetorno;
     },
 
-    getRevenue(revenue) {
-        let revenueId = revenue.id;
-        return db(TABLE_NAME).where('id', revenueId).select('');
+    async getRevenueId(revenueId) {
+        let objRetorno = {};
+        objRetorno.status = false;
+        objRetorno.code = 500;
+
+        await db(TABLE_NAME)
+            .select('*')
+            .where('id', revenueId)
+            .then(result => {
+                objRetorno.code = 200;
+                objRetorno.status = true;
+                objRetorno.data = result;
+            })
+            .catch(err => {
+                objRetorno.data = err;
+            });
+
+        return objRetorno;
+    },
+
+    async getRevenueName(revenueName) {
+        let objRetorno = {};
+        console.log('revenueName: ' + JSON.stringify(revenueName));
+        await db(TABLE_NAME)
+            .select('id')
+            .where(revenueName)
+            .then(result => {
+                console.log('result getByName: ' + JSON.stringify(result));
+                objRetorno.data = result;
+            })
+            .catch(err => {
+                objRetorno.data = err;
+            });
+        console.log('objRetorno:' + JSON.stringify(objRetorno.data[0].id));
+        return objRetorno.data[0].id;
     },
 
     newRevenue(revenue) {
         let obj = { name: revenue.fname, user: 'lucas.paula' }
-        return db.insert(obj).into(TABLE_NAME)
-        .then(function () {
-        });
+        //return db.insert(obj).into(TABLE_NAME)
+        return db(TABLE_NAME)
+            .insert(obj)
+            .then(function () {
+            });
     },
 
-    delRevenue(revenue) {
-        return db(TABLE_NAME)
-            .where('id', revenue.id)
-            .del();
+    async delRevenue(revenue) {
+        let obj_name = { name: revenue.fname };
+        let exit;
+        console.log('obj_name: ' + JSON.stringify(obj_name));
+
+        await this.getRevenueName(obj_name)
+            .then(result => {
+                console.log('result: ' + JSON.stringify(result));
+                let obj = { id: result };
+                console.log('obj: ' + JSON.stringify(obj));
+                exit = db(TABLE_NAME)
+                    .where(obj)
+                    .del()
+                    .then(function () {
+                    });
+            })
+            .catch(err => {
+                exit = err;
+            });
+        console.log('exit: ' + JSON.stringify(exit) );
+        return exit;
     },
 
     updateRevenue(revenue) {
         return db(TABLE_NAME)
             .where('id', revenue.id)
             .update({
-                name: revenue.nome,
-                description: revenue.descricao,
-                price: revenue.preco
+                name: revenue.fname,
+                user: 'lucas.paula'
+            })
+            .then(function () {
             });
     }
 }
