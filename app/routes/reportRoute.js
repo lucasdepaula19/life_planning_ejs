0f1ef7172b443dda5b953f15a51c07f0e786c0aa
Lucas de Paula <45231571+lucasdepaula19@users.noreply.github.com>
2019-11-22 15:07:46 -0300

    detalhes

--- app/routes/reportRoute.js
+++ app/routes/reportRoute.js
@@ -5,12 +5,10 @@ const Category = require('../controllers/CategoryController');
 /* GET users listing. */
 router.get('/', function (req, res, next) {
 
-  //let obj_category;
-
   Category.getCategory()
     .then(result => {
-      res.render('report', { obj_category: result.data });
-    })
+    res.render('report', { obj_category: result.data });
+  })
     .catch(err => {
       res.status(err.code).send(err);
     });
