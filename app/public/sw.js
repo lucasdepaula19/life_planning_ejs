e3d0834ab44f492c3d42bd3f8c4a9e88a4188d28
Lucas de Paula <lucasdepaula2012@gmail.com>
2019-11-23 20:43:53 -0300

    ajustes sw

--- app/public/sw.js
+++ app/public/sw.js
@@ -3,7 +3,14 @@
 const CACHE = "pwabuilder-page";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
+const offlineFallbackPage = [
+  '/',
+  '../views/index.ejs',
+  '/stylesheets/styles.css',
+  '/javascripts/main.js',
+  '/manifest.json',
+  '/sw.js',
+];
 
 // Install stage sets up the offline page in the cache and opens a new cache
 self.addEventListener("install", function (event) {
@@ -13,7 +20,7 @@ self.addEventListener("install", function (event) {
     caches.open(CACHE).then(function (cache) {
       console.log("[PWA Builder] Cached offline page during install");
 
-      if (offlineFallbackPage === "ToDo-replace-this-name.html") {
+      if (offlineFallbackPage === "index.ejs") {
         return cache.add(new Response("TODO: Update the value of the offlineFallbackPage constant in the serviceworker."));
       }
 
